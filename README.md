3d8f563f6dc44d6f2f7fbd69fee74cc734d6a871
DevonMartens <dmmartens91@gmail.com>
2021-01-08 20:00:53 -0500

    button starts game

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # pokemon-fight
+
+
+Test it out
+
+https://devonmartens.github.io/pokemon-fight/
--- assets/js/button.js
+++ assets/js/button.js
@@ -2,3 +2,8 @@ $("#go").click(function() {
     $("html, body").animate({ scrollTop: $(document).height() }, "slow");
 
   });​
+
+  $("#go").click(function() {
+  location.href="./assets/html/game"
+
+  });​
