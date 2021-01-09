167473c0f0fc5f9a04c6746ff327477ec57a8fd6
DevonMartens <dmmartens91@gmail.com>
2021-01-09 09:18:39 -0500

    first ajax call work, dummy pokemon card on screen setting up places for ajax response to go

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,25 +1,41 @@
-//hard coded pokemon array to procide suggestions
-var pokemonArray = [];
-//blank input for response
-var calledPokemon;
-var pokemonTeam = [];
 
+var g_currentpokemon;
+var userTeam = [];
 $("#search-btn").on("click", function () {
-  event.preventDefault();
+  // event.preventDefault();
+  var userInput = $("#input").val();
+
   var userInput = $("#input").val();
   var queryURL = "https://pokeapi.co/api/v2/pokemon/" + userInput;
-  $.ajax({
+ $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (response) {
-console.log(response);
     g_currentpokemon = response;
-
+console.log(response);
     var pokeID = response.id;
     var imageURL =
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" +
       pokeID +
       ".png";
-   })
-})
+    var nameOfPokemon = response.name;
+
+    nameOfPokemon = nameOfPokemon.charAt(0).toUpperCase() + nameOfPokemon.slice(1);
+
+    var type = response.types[0].type.name;
+    type = type.charAt(0).toUpperCase() + type.slice(1);
+
+    $("#pokemon1Image").html("<div><img src=" + imageURL + "></div>");
+    $("#pokemon1Name").text(nameOfPokemon);
+    $("#poketype").text(type + " type");
+
+    $("#pokemon1Attack").text("Attack stat: " + response.stats[1].base_stat);
+    $("#Pokemon1Defense").text("Defense stat: " + response.stats[2].base_stat);
+    $("#Pokemon1Speed").text("Speed stat: " + response.stats[5].base_stat);
+    $("#Pokemon1Exp").text(response.base_experience);
+
+  
+  })
+});
+
 
